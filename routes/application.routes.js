bd5530d0b347c1266b64936c1ab7e75a4d3029ee
Kunal Kumar <kunal23kmr@gmail.com>
2024-03-29 12:12:42 +0530

    Minor changes.

--- routes/application.routes.js
+++ routes/application.routes.js
@@ -15,7 +15,7 @@ router.post('/create', isLoggedIn, authorizeRoles('DS'), createApplication);
 router.patch('/update/:id', isLoggedIn, authorizeRoles('DS'), updateApplication);
 router.delete('/delete/:id', isLoggedIn, authorizeRoles('DS'), deleteApplication);
 router.post('/apply/:id', isLoggedIn, applyToApplication);
-router.post('/getAllJobs', isLoggedIn, getAllJobs);
+router.post('/getAllJobs', getAllJobs);
 router.post('/getApplicationById/:jobId', getApplicationByJobId);
 
 export default router;
