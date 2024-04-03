24abf65dea3092e9aa9f595134a863f833153922
Kunal Kumar <kunal23kmr@gmail.com>
2024-04-04 00:00:02 +0530

    done with application review by DS

--- controllers/form.controller.js
+++ controllers/form.controller.js
@@ -10,7 +10,7 @@ import User from "../models/user.model.js";
 
 // })
 
-export const getFormResponseByJobId = asyncHandler(async (req, res, next) => {
+export const getUserFormResponseByJobId = asyncHandler(async (req, res, next) => {
     const { jobId } = req.params;
     const userId = req.user.id;
 
@@ -63,6 +63,37 @@ export const getFormResponseByJobId = asyncHandler(async (req, res, next) => {
     }
 });
 
+
+
+export const getAllFormResponseByJobId = asyncHandler(async (req, res, next) => {
+    try {
+        const { jobId } = req.params;
+        const application = await Application.findOne({ jobId });
+
+        if (!application) {
+            return next(new AppError('Application not found',409));
+        }
+
+        // get all the fromIds of response received on that requested application. 
+        const formIds = application.appliedBy.map(item => item.form);
+
+        // Find all forms with the extracted formIds
+        const forms = await Form.find({ _id: { $in: formIds } });
+
+        // Filter out null/undefined values
+        const filteredForms = forms.filter(form => form);
+
+        res.status(200).json({
+            success: true,
+            forms: filteredForms,
+            message:'From data fetched successfully.'
+        });
+    } catch (err) {
+        console.log(err);
+        return next(new AppError('Error occured.', 505));
+    }
+
+})
 
 
 // export const createApplication = asyncHandler(async (req, res, next) => {
--- routes/form.routes.js
+++ routes/form.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { authorizeRoles, isLoggedIn } from "../middlewares/auth.middleware.js";
-import { getFormResponseByJobId } from "../controllers/form.controller.js";
+import { getUserFormResponseByJobId, getAllFormResponseByJobId } from "../controllers/form.controller.js";
 const router = Router();
 
 
 
 // router.post('/create', isLoggedIn, authorizeRoles('DS'), createApplication);
 
-router.post('/:jobId', isLoggedIn, authorizeRoles('USER'), getFormResponseByJobId);
+router.post('/:jobId', isLoggedIn, authorizeRoles('USER'), getUserFormResponseByJobId);
+router.post('/getAllFormResponseByJobId/:jobId', isLoggedIn, getAllFormResponseByJobId);
 
 export default router;
